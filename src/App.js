843ce7e78359101afbb26b64bca419ffcac9e014
nency <snency16@gmail.com>
2024-04-05 13:48:54 +0530

    About us

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
 <>
   <Navbar title='TextUtils' home='Home' about='About Us' ser1='Service1' ser2='Service2' more='More'/>
     {/* <Navbar /> */}
-    <div className='container my-3'>
+    {/* <div className='container my-3'>
     <TextForm heading='TextUtils :)' />
+    </div> */}
+    <div className='container my-3'>
+      <About />
     </div>
 </>
   );
